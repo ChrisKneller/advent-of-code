57f0e1c8dcd2d8f7e69ce745c2a117568bcec8a1
Christian Kneller <christian.kneller@partners-cap.com>
2022-12-05 08:51:33 +0000

    Shorten

--- 2022/3/solution_1.py
+++ 2022/3/solution_1.py
@@ -5,7 +5,7 @@ def solve():
     with open('input.txt', 'r') as f:
 
         # 130 characters
-        return r(lambda x, y: x + p.find(set(y[:len(y)//2]).intersection((set(y[len(y)//2:len(y)]))).pop()) + 1, f.read().splitlines(), 0)
+        return r(lambda x, y: x + p.find(set(y[:len(y)//2]).intersection((set(y[len(y)//2:len(y)]))).pop()) + 1, f.readlines(), 0)
 
 if __name__ == "__main__":
     print(solve())
